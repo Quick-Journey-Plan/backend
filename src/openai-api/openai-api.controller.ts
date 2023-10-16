cd27ef6d5e6793776f06664d746703ea1e318c6b
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-16 21:33:27 +0200

    add: dotev config and basic openai api request

--- src/openai-api/openai-api.controller.ts
+++ src/openai-api/openai-api.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { OpenaiApiService } from './openai-api.service';
 import { CreateOpenaiApiDto } from './dto/create-openai-api.dto';
+import OpenAI from 'openai';
 
 @Controller('openai-api')
 export class OpenaiApiController {
   constructor(private readonly openaiApiService: OpenaiApiService) { }
 
   @Post()
-  public async getCityInfo(@Body() createOpenaiApiDto: CreateOpenaiApiDto): Promise<string> {
+  public async getCityInfo(@Body() createOpenaiApiDto: CreateOpenaiApiDto): Promise<OpenAI.Chat.Completions.ChatCompletion.Choice[]> {
     return await this.openaiApiService.getCityInfo(createOpenaiApiDto);
   }
 
--- src/openai-api/openai-api.service.ts
+++ src/openai-api/openai-api.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { CreateOpenaiApiDto } from './dto/create-openai-api.dto';
 import OpenAI from 'openai';
 
-
 @Injectable()
 export class OpenaiApiService {
 
-  public async getCityInfo(createOpenaiApiDto: CreateOpenaiApiDto): Promise<string> {
+  public async getCityInfo(createOpenaiApiDto: CreateOpenaiApiDto): Promise<OpenAI.Chat.Completions.ChatCompletion.Choice[]> {
     // @TODO: first check the city in DB
 
-    // @TODO: get datas from OpenAI API
     const openai = new OpenAI({
-      apiKey: 'my api key', // defaults to process.env["OPENAI_API_KEY"]
+      apiKey: process.env.OPENAI_API_KEY,
     });
 
-    return await 'This action adds a new openaiApi';
-  }
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: `give me 3 days plan in ${createOpenaiApiDto.city}` }],
+      model: 'gpt-3.5-turbo',
+    });
 
+    return chatCompletion.choices;
+  }
 }
