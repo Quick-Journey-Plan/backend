9475aca4ab8285bfe3b2ce7931bc37843abfba52
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-17 07:15:05 +0200

    add: auth and users module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { OpenaiApiModule } from './openai-api/openai-api.module';
-
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [OpenaiApiModule, ConfigModule.forRoot()],
+  imports: [OpenaiApiModule, ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
