9e53dfdea950c0b9b9c4b52d4e4c00156f2ba9dd
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-17 07:27:01 +0200

    add: export UsersService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private usersService: UsersService) { }
+
+    public async signIn(username: string, password: string): Promise<string> {
+        // @TODO: check user exists in DB
+
+        return 'Sign in was successfull';
+    }
+}
