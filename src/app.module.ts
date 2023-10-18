f2bb767a506916d5c208685078d79bfce00cd677
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-18 21:37:13 +0200

    add: TypeOrm and DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { OpenaiApiModule } from './openai-api/openai-api.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [OpenaiApiModule, ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USER,
+      entities: [],
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
+    OpenaiApiModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-
-    constructor(private usersService: UsersService) { }
 
     public async signIn(username: string, password: string): Promise<string> {
         // @TODO: check user exists in DB
