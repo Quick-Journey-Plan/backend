4e90a08bc5ab4c41c7cfe0d9de59694be07c2a59
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-11-06 21:23:45 +0100

    update: remove unused dependency

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
